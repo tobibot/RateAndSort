ebef248ef8d63315587f85dbe2e381d51a7cfb3b
tobibot <tobias.heying@outlook.com>
2021-05-07 23:40:46 +0200

    first implementation

---
+++ rateAndSort/rateAndSort.go
@@ -0,0 +1,137 @@
+package rateAndSort
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"path"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	inputFileName = "evaluation.json" // todo args / flags
+	environment   = "prod"
+	envFileName   = environment + ".env"
+)
+
+type stock struct {
+	Symbol string
+	Name   string
+	Value  int
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func StartRating() {
+	entries, err := readEvaluationFile()
+	if err != nil {
+		log.Fatal()
+	}
+
+	playTheGame(entries)
+}
+
+func playTheGame(stocks []stock) {
+	for {
+		randomize(stocks)
+
+		for i := 0; i < len(stocks); i += 2 {
+			if i+1 == len(stocks) {
+				continue
+			}
+			stock1 := &stocks[i]
+			stock2 := &stocks[i+1]
+			msg := fmt.Sprintf("Which one do you prefer?\n"+
+				"\t(a) %s or\n"+
+				"\t(b) %s\n", stock1.Name, stock2.Name)
+			fmt.Println(msg)
+
+			text := ""
+			fmt.Scanln(&text)
+
+			if text == "a" {
+				stock1.Value += 1
+				stock2.Value -= 1
+				writeEvaluationFile(stocks)
+			} else if text == "b" {
+				stock1.Value -= 1
+				stock2.Value += 1
+				writeEvaluationFile(stocks)
+			} else if text == "x" || text == "quit" || text == "exit" {
+				//sort(stocks) // Todo
+				//writeEvaluationFile(stocks)
+				return
+			} else {
+				fmt.Println("bad input")
+			}
+		}
+	}
+
+}
+
+func writeEvaluationFile(data []stock) (err error) {
+	_ = godotenv.Load(envFileName)
+
+	finFolder := os.Getenv("FINFOLDER")
+	filePath := path.Join(finFolder, environment, "config", inputFileName)
+
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "\t")
+
+	err = encoder.Encode(data)
+	return err
+}
+
+func readEvaluationFile() ([]stock, error) {
+	_ = godotenv.Load(envFileName)
+
+	finFolder := os.Getenv("FINFOLDER")
+	filePath := path.Join(finFolder, environment, "config", inputFileName)
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		os.Create(filePath)
+		return make([]stock, 0), err
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return make([]stock, 0), err
+	}
+
+	defer file.Close()
+	byteValue, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		return make([]stock, 0), err
+	}
+
+	data := make([]stock, 0)
+
+	if len(byteValue) == 0 {
+		return make([]stock, 0), fmt.Errorf("file contains no data")
+	}
+
+	err = json.Unmarshal(byteValue, &data)
+	if err != nil {
+		return make([]stock, 0), err
+	}
+	return data, nil
+}
+
+func randomize(sliceIn []stock) {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(sliceIn), func(i, j int) { sliceIn[i], sliceIn[j] = sliceIn[j], sliceIn[i] })
+}
---
+++ rateAndSort/rateAndSort_test.go
@@ -0,0 +1,35 @@
+package rateAndSort
+
+import (
+	"testing"
+)
+
+func Test_writeEvaluationFile(t *testing.T) {
+	environment = "test"
+	envFileName = "test.env"
+
+	type args struct {
+		data []stock
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "write sample file",
+			args: args{data: []stock{
+				{Symbol: "BAYN.de", Value: -5},
+				{Symbol: "DTE.DE", Value: 5},
+			}},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := writeEvaluationFile(tt.args.data); (err != nil) != tt.wantErr {
+				t.Errorf("writeEvaluationFile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
