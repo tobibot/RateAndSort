a52c87ff03a674f5f122a4cd9bb689d1715d9aca
tobibot <tobias.heying@outlook.com>
2021-05-07 23:44:15 +0200

    first implementation

--- main.go
+++ main.go
@@ -1,45 +1,7 @@
 package main
 
-import (
-	"log"
-	"math/rand"
-	"time"
-)
-
-var (
-	inputFileName = "input.json" // todo args / flags
-)
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
+import "github.com/tobibot/RateAndSort/rateAndSort"
 
 func main() {
-
-	entries, err := readInputFile(inputFileName)
-	if err != nil {
-		log.Fatal()
-	}
-
-	playTheGame(entries)
-
-}
-
-func readInputFile(fileName string) ([]interface{}, error) {
-	return nil, nil
-}
-
-func playTheGame(entries []interface{}) {
-
-	for {
-		for _, entry := range entries {
-			// save order after each evaluation
-
-			_ = entry
-		}
-
-		// todo if input == x, quit, exit => exit
-
-	}
-
+	rateAndSort.StartRating()
 }
